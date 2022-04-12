9916f2f65853f6548fa78d7bedcfdd6d24e73c08
zexianliOSU <lizexi@oregonstate.edu>
2022-04-12 13:10:58 -0700

    Boiler plate

---
+++ VoxelEngine/src/texture/textureAtlas.h
@@ -0,0 +1,18 @@
+#ifndef TEXTUREATLAS_H
+#define TEXTUREATLAS_H
+
+class TextureAtlas {
+public:
+	TextureAtlas();
+	~TextureAtlas();
+private:
+
+};
+
+TextureAtlas::TextureAtlas() {
+}
+
+TextureAtlas ::~TextureAtlas() {
+}
+
+#endif
